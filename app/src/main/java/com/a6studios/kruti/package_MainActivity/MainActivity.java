627ab3f9bb796db36bca4d9b84310c885356266d
ksasanka11 <30541568+ksasanka11@users.noreply.github.com>
2018-03-26 11:36:42 +0530

    uploading image to profile( not from camera )

--- app/src/main/java/com/a6studios/kruti/package_MainActivity/MainActivity.java
+++ app/src/main/java/com/a6studios/kruti/package_MainActivity/MainActivity.java
@@ -4,13 +4,11 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,7 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     ViewPager textSlider;
     TextSliderAdapter textSliderAdapter;
-    LinearLayout linearLayout;
+    LinearLayout textSlider_ll;
     LinearLayout ask_query_ll, chat, profile, call;
     TextView ask_query_text, chat_text, profile_text, call_text;
     CardView ask_query_cv, call_cv;
@@ -35,9 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textSlider = (ViewPager) findViewById(R.id.textSlider);
-        linearLayout = (LinearLayout) findViewById(R.id.textSliderLayout);
 
         //ask_query
         ask_query_ll = (LinearLayout) findViewById(R.id.ask_query_ll);
@@ -65,10 +60,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         profile.setOnClickListener(this);
 
 
+        //textSlider
+        textSlider = (ViewPager) findViewById(R.id.textSlider);
+        textSlider_ll = (LinearLayout) findViewById(R.id.textSliderLayout);
         textSliderAdapter = new TextSliderAdapter(this);
-
         textSlider.setAdapter(textSliderAdapter);
-
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new SliderTimer(), 2000, 4000);
     }
@@ -81,14 +77,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         if (view == call || view == call_cv || view == call_text) {
-            /*if (isPermissionGranted()) {
-                call_action();
-            }*/
 
             String phnum = "1234567890";
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(Uri.parse("tel:" + phnum));
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                    != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
                 // here to request the missing permissions, and then overriding
@@ -119,8 +113,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return;
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
@@ -133,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 public void run() {
                     int i = textSliderAdapter.getCount();
                     int j = textSlider.getCurrentItem();
-                    linearLayout.setOnClickListener(new View.OnClickListener() {
+                    textSlider_ll.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             try {
